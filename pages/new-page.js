548d4f487122eeb3f84f697d52f801dfb406d5e9
charon1102 <yangxu986002704@163.com>
2024-04-17 17:46:08 +0900

    Add new-page.js and update about page

---
+++ pages/new-page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NewPage = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <img src="/gougou.jpg" alt="This is My dog" />
+      <p>Hi, I'm [YANG XU]. I'm a [student] based in [korean]. Nice to meet you!</p>
+    </div>
+  );
+}
+
+export default NewPage;
